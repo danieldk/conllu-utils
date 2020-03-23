23d0e3532a0d49861d2d1d077fa8beaf6ad93db7
Daniël de Kok <me@danieldk.eu>
2020-03-23 07:56:55 +0100

    Add merge subcommand

--- src/io.rs
+++ src/io.rs
@@ -1,12 +1,14 @@
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io;
-use std::io::{BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 
+use conllu::io::{Reader, Writer};
+use flate2::read::GzDecoder;
 use flate2::write::GzEncoder;
 
-pub fn create_writer<P>(filename: P, gzip: bool) -> io::Result<conllu::io::Writer<Box<dyn Write>>>
+pub fn create_writer<P>(filename: P, gzip: bool) -> io::Result<Writer<Box<dyn Write>>>
 where
     P: AsRef<Path>,
 {
@@ -17,10 +19,25 @@ where
         Box::new(BufWriter::new(file))
     };
 
-    Ok(conllu::io::Writer::new(boxed_writer))
+    Ok(Writer::new(boxed_writer))
 }
 
-pub fn open_writer<P>(path: &P) -> io::Result<conllu::io::Writer<Box<dyn Write>>>
+pub fn open_reader<P>(path: &P) -> io::Result<Reader<Box<dyn BufRead>>>
+where
+    P: AsRef<Path>,
+{
+    let f = File::open(path)?;
+
+    let boxed_reader: Box<dyn BufRead> = if path.as_ref().extension() == Some(OsStr::new("gz")) {
+        Box::new(BufReader::new(GzDecoder::new(f)))
+    } else {
+        Box::new(BufReader::new(f))
+    };
+
+    Ok(Reader::new(boxed_reader))
+}
+
+pub fn open_writer<P>(path: &P) -> io::Result<Writer<Box<dyn Write>>>
 where
     P: AsRef<Path>,
 {
