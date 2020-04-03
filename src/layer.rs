09c5f106e365ae0518e2b911881d5f7e13d1829b
Daniël de Kok <me@danieldk.eu>
2020-04-03 11:37:00 +0200

    Add accuracy subcommand

--- src/layer.rs
+++ src/layer.rs
@@ -9,6 +9,7 @@ pub fn layer_callback(layer: &str) -> Option<LayerCallback> {
         "features" => Some(Box::new(|t| Some(Cow::Owned(t.features().to_string())))),
         "form" => Some(Box::new(|t| Some(Cow::Borrowed(t.form())))),
         "lemma" => Some(Box::new(|t| t.lemma().map(Cow::Borrowed))),
+        "misc" => Some(Box::new(|t| Some(Cow::Owned(t.misc().to_string())))),
         "upos" => Some(Box::new(|t| t.upos().map(Cow::Borrowed))),
         "xpos" => Some(Box::new(|t| t.xpos().map(Cow::Borrowed))),
         _ => None,
